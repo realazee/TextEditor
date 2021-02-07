60d45772feca15cdcd4759605d0f8c9ea099c58a
Aaron Zheng <100024608@mvla.net>
2021-02-06 16:30:15 -0800

    Updated threshold to 35000, rechecked grader output.

--- src/spelling/NearbyWords.java
+++ src/spelling/NearbyWords.java
@@ -17,7 +17,7 @@ public class NearbyWords implements SpellingSuggest {
 	// THRESHOLD to determine how many words to look through when looking
 	// for spelling suggestions (stops prohibitively long searching)
 	// For use in the Optional Optimization in Part 2.
-	private static final int THRESHOLD = 1000; 
+	private static final int THRESHOLD = 35000; 
 
 	Dictionary dict;
 
